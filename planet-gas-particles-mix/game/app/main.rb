5888ab9aa02cf180c97b1dc8e634484c91de3829
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-07 13:11:08 +0900

    Add particle

--- planet-gas-particles-mix/game/app/main.rb
+++ planet-gas-particles-mix/game/app/main.rb
@@ -2,6 +2,8 @@ require 'lib/debug_mode.rb'
 require 'lib/resources.rb'
 require 'lib/sprite_resources.rb'
 
+require 'app/resources.rb'
+
 def tick(args)
   # Game
 end
---
+++ planet-gas-particles-mix/game/app/resources.rb
@@ -0,0 +1,6 @@
+# Game Resources
+module Resources
+  group :sprites, 'png' do
+    add :particle, w: 512, h: 512
+  end
+end
