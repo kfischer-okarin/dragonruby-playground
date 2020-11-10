f04111e4b99adb1b53211d85eb58ffb1b98a3f6d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-10 21:24:33 +0900

    Add UI resources

--- planet-gas-particles-mix/game/app/resources.rb
+++ planet-gas-particles-mix/game/app/resources.rb
@@ -2,5 +2,8 @@
 module Resources
   group :sprites, 'png' do
     add :particle, w: 512, h: 512, author: 'Kenney', source: 'Assets 3/2D/Particles/circle_05'
+    add :panel, w: 128, h: 128, author: 'Kenney', source: 'Assets 3/2D/Interface/panel_boltsRed'
+    add :scrollbar, w: 128, h: 32, author: 'Kenney', source: 'Assets 3/2D/Interface/scrollbar_barRound'
+    add :scrollbar_thumb, w: 40, h: 32, author: 'Kenney', source: 'Assets 3/2D/Interface/scrollbar_metalSmall'
   end
 end
