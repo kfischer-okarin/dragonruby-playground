7ac70fb98ad0de9cc4cda6c3239a67af6666c3e7
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-12-22 22:34:58 +0900

    Add play_sample method to outputs

--- twinstick-shooter/game/app/game/outputs.rb
+++ twinstick-shooter/game/app/game/outputs.rb
@@ -5,15 +5,33 @@ class Game
   class Outputs
     def initialize
       @canvas = DRT::LowResolutionCanvas.new([Game::W, Game::H])
+      @samples = []
+      @audio_id = 0
     end
 
     def draw(primitive)
       @canvas.primitives << primitive
     end
 
+    def play_sample(sample)
+      @samples << sample
+    end
+
     def process(args)
       args.outputs.background_color = [0, 0, 0]
       args.outputs.primitives << @canvas
+
+      play_next_sample(args) until @samples.empty?
+    end
+
+    private
+
+    def play_next_sample(args)
+      sample = @samples.pop
+      args.audio[@audio_id] = {
+        input: [1, 48000, sample]
+      }
+      @audio_id += 1
     end
   end
 end
