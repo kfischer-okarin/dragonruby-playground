3d32ed093db15982e2c5c67a7759c5311a348a42
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-12-23 22:47:21 +0900

    Rewrite Synthesizer

--- twinstick-shooter/game/app/sonic_gun.rb
+++ twinstick-shooter/game/app/sonic_gun.rb
@@ -39,54 +39,82 @@ class SonicGun
     )
   end
 
+  class Synthesizer
+    def self.generate_sine_wave(sample_rate, frequency, length)
+      period_length = (sample_rate / frequency).ceil
+      one_period = period_length.map_with_index { |i|
+        Math.sin((2 * Math::PI) * (i / period_length))
+      }
+      one_period * (frequency * length).ceil
+    end
+
+    def initialize(sample_rate)
+      @sample_rate = sample_rate
+      @result = []
+    end
+
+    def generate
+      @result
+    end
+
+    def sine_wave(frequency, length)
+      @frequency = frequency
+      @length = length
+      @result = self.class.generate_sine_wave(@sample_rate, frequency, length)
+      self
+    end
+
+    def add_harmonics_up_to(n)
+      (2..n).each do |harmonic|
+        sine = self.class.generate_sine_wave(@sample_rate, @frequency * harmonic, @length)
+        @result.size.times do |k|
+          @result[k] += sine[k]
+        end
+      end
+      self
+    end
+
+    def envelope_adsr(attack, decay, sustain, release)
+      full_amplitude_index = (attack * @sample_rate).ceil
+      sustain_amplitude_index = full_amplitude_index + (decay * @sample_rate).ceil
+      release_index = @result.size - (release * @sample_rate).ceil
+      @result.each_with_index do |value, index|
+        @result[index] = value * if index <= full_amplitude_index
+                                   index / full_amplitude_index
+                                 elsif index <= sustain_amplitude_index
+                                   progress = (index - full_amplitude_index) / (sustain_amplitude_index - full_amplitude_index)
+                                   1 * (1 - progress) + sustain * progress
+                                 elsif index <= release_index
+                                   sustain
+                                 else
+                                   progress = (index - release_index) / (@result.size - release_index)
+                                   sustain * (1 - progress)
+                                 end
+                                end
+      self
+    end
+
+    def normalize(amplitude = 1.0)
+      factor = amplitude / @result.max
+      @result.size.times do |k|
+        @result[k] *= factor
+      end
+      self
+    end
+  end
+
   SAMPLE_RATE = 48000
 
-  def self.generate_sine(frequency, length)
-    period_length = (SAMPLE_RATE / frequency).ceil
-    one_period = period_length.map_with_index { |i|
-      Math.sin((2 * Math::PI) * (i / period_length))
-    }
-    one_period * (frequency * length).ceil
-  end
-
-  def self.adsr(sound, attack, decay, sustain, release)
-    full_amplitude_index = (attack * SAMPLE_RATE).ceil
-    sustain_amplitude_index = full_amplitude_index + (decay * SAMPLE_RATE).ceil
-    release_index = sound.size - (release * SAMPLE_RATE).ceil
-    sound.map_with_index { |value, index|
-      value * if index <= full_amplitude_index
-                index / full_amplitude_index
-              elsif index <= sustain_amplitude_index
-                progress = (index - full_amplitude_index) / (sustain_amplitude_index - full_amplitude_index)
-                1 * (1 - progress) + sustain * progress
-              elsif index <= release_index
-                sustain
-              else
-                progress = (index - release_index) / (sound.size - release_index)
-                sustain * (1 - progress)
-              end
-    }
-  end
-
   def self.bullet_sound
-    @bullet_sound ||= adsr(normalize(generate_bullet_sound, 0.1), 0.05, 0.05, 0.6, 0.5)
+    @bullet_sound ||= generate_bullet_sound
   end
 
   def self.generate_bullet_sound
-    length = 0.25
-    base_f = 440
-    result = [0] * (length * SAMPLE_RATE)
-    (1..5).each do |harmonic|
-      sine = generate_sine(base_f * harmonic, length)
-      result.size.times do |k|
-        result[k] += sine[k]
-      end
-    end
-    result
-  end
-
-  def self.normalize(sound, amplitude = 1.0)
-    max = sound.max
-    sound.map { |val| val * amplitude / max }
+    Synthesizer.new(48000)
+               .sine_wave(440, 0.25)
+               .add_harmonics_up_to(20)
+               .normalize(0.1)
+               .envelope_adsr(0.05, 0.05, 0.6, 0.5)
+               .generate
   end
 end
