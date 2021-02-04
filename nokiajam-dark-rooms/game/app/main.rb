7b91467947366621be81ca4122fd3e3efcd1e9b9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-02-04 22:22:25 +0900

    Light beam

--- nokiajam-dark-rooms/game/app/main.rb
+++ nokiajam-dark-rooms/game/app/main.rb
@@ -25,21 +25,27 @@ end
 
 def wall_render_options(room, wall)
   {}.tap { |result|
-    result.update ROTATED_BY_90_DEGREES if %i[left right].include?(wall)
+    result.update(angle: 90, angle_anchor_x: 0, angle_anchor_y: 0.5) if %i[left right].include?(wall)
     result[:invert] = true if room[:light]
   }
 end
 
-ROTATED_BY_90_DEGREES = { angle: 90, angle_anchor_x: 0, angle_anchor_y: 0.5 }.freeze
-
 def render_room(room)
   left = ($outputs.w - Sprites::WALL_LENGTH).half
+  right = left + Sprites::WALL_LENGTH - 1
   bottom = ($outputs.h - Sprites::WALL_LENGTH).half
+  top = bottom + Sprites::WALL_LENGTH - 1
   $outputs.render_sprite left, bottom, wall_type(room, :down), wall_render_options(room, :down)
-  $outputs.render_sprite left, bottom + Sprites::WALL_LENGTH - 1, wall_type(room, :up), wall_render_options(room, :up)
+  $outputs.render_sprite left, top, wall_type(room, :up), wall_render_options(room, :up)
   $outputs.render_sprite left, bottom - 1, wall_type(room, :left), wall_render_options(room, :left)
-  $outputs.render_sprite left + Sprites::WALL_LENGTH - 1, bottom - 1, wall_type(room, :right), wall_render_options(room, :right)
-  $outputs.render_rect left + 1, bottom + 1, Sprites::WALL_LENGTH - 2, Sprites::WALL_LENGTH - 2 if room[:light]
+  $outputs.render_sprite right, bottom - 1, wall_type(room, :right), wall_render_options(room, :right)
+  return unless room[:light]
+
+  $outputs.render_rect left + 1, bottom + 1, Sprites::WALL_LENGTH - 2, Sprites::WALL_LENGTH - 2
+  $outputs.render_sprite left + 7, top, :light_beam if room[:up]
+  $outputs.render_sprite left + 1, bottom + 7, :light_beam, angle: 90, angle_anchor_x: 0, angle_anchor_y: 0 if room[:left]
+  $outputs.render_sprite right, bottom + 22, :light_beam, angle: 270, angle_anchor_x: 0, angle_anchor_y: 0 if room[:right]
+  $outputs.render_sprite left + 22, bottom + 1, :light_beam, angle: 180, angle_anchor_x: 0, angle_anchor_y: 0 if room[:down]
 end
 
 def tick(args)
--- nokiajam-dark-rooms/game/app/sprites.rb
+++ nokiajam-dark-rooms/game/app/sprites.rb
@@ -7,7 +7,19 @@ module Sprites
     wall_with_door: ['X' * HALF_WALL_LENGTH + ' ' * DOOR_SIZE + 'X' * HALF_WALL_LENGTH],
     wall: ['X' * WALL_LENGTH],
     wall_with_door_dark: ['XX X X X X         X X X X XX'],
-    wall_dark: ['XX X X X X X X X X X X X X XX']
+    wall_dark: ['XX X X X X X X X X X X X X XX'],
+    light_beam: [
+      '     X   X     ',
+      '       X       ',
+      '  X XXXXXXX X  ',
+      '   XXX X XXX   ',
+      'X XX XXXXX XX X',
+      ' XXXXXXXXXXXXX ',
+      ' X XXXXXXXXX X ',
+      '  XXXXXXXXXXX  ',
+      '  XXXXXXXXXXX  ',
+      '   XXXXXXXXX   '
+    ]
   }.freeze
 
   def self.prepare
