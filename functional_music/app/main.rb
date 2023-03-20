804b38c9ad1f828e115f38d22a1dd88d0fcda6cc
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-03-21 02:21:18 +0900

    Implement bell

--- functional_music/app/main.rb
+++ functional_music/app/main.rb
@@ -4,7 +4,11 @@ require 'app/dr_interface.rb'
 def tick(args)
   # args.audio[:sound] ||= build_sound tone(300)
   # args.audio[:sound] ||= build_sound doubletone(300, 300.5)
-  args.audio[:sound] ||= build_sound beep(300)
+  # args.audio[:sound] ||= build_sound beep(300)
+  # args.audio[:sound] ||= build_sound bell(300)
+  # args.audio[:sound] ||= build_sound bell(600)
+  # args.audio[:sound] ||= build_sound bell(500, 10, 0)
+  args.audio[:sound] ||= build_sound bell(400, 10, 0, 0)
 end
 
 def tone(frequency = 440)
@@ -18,6 +22,15 @@ end
 def beep(frequency = 440, duration = 1)
   envelope = line(1, 0, duration)
   tone(frequency) * envelope
+end
+
+def bell(frequency = 440, duration = 10, h0 = 1, h1 = 0.6, h2 = 0.4, h3 = 0.25, h4 = 0.2, h5 = 0.15)
+  harmonic_series = [1, 2, 3, 4.2, 5.4, 6.8]
+  proportions = [h0, h1, h2, h3, h4, h5]
+  components = harmonic_series.zip(proportions).map do |harmonic, proportion|
+    tone(frequency * harmonic) * perc(release: proportion * duration, level: proportion * 0.5)
+  end
+  mix(components)
 end
 
 $gtk.reset
--- functional_music/app/music.rb
+++ functional_music/app/music.rb
@@ -25,6 +25,10 @@ module ComposableSoundGenerator
   end
 end
 
+def mix(sound_generators)
+  sound_generators.reduce { |mixed_so_far, generator| mixed_so_far + generator }
+end
+
 def sin_osc(frequency)
   ComposableSoundGenerator.define do |t|
     Math.sin(t * frequency * Math::PI * 2) * 0.5
@@ -34,5 +38,48 @@ end
 def line(start_value, end_value, duration)
   ComposableSoundGenerator.define do |t|
     t < duration ? start_value : end_value
+  end
+end
+
+def perc(attack: 0.01, release: 1, level: 1, curve: -4)
+  envelope([0, level, 0], [attack, release], curve)
+end
+
+def envelope(levels, durations, curve)
+  t = 0
+  segments = []
+  levels.each_cons(2).zip(durations).each do |(from_level, to_level), duration|
+    t_max = t + duration
+    segments << {
+      curve: interpolation_curve(t_min: t, t_max: t_max, out_min: from_level, out_max: to_level, curve: curve),
+      t_max: t_max
+    }
+    t = t_max
+  end
+
+  ComposableSoundGenerator.define do |t|
+    segment = segments.find { |s| t < s[:t_max] }
+
+    segment ? segment[:curve].call(t) : levels.last
+  end
+end
+
+def interpolation_curve(t_min: 0, t_max: 1, out_min: 0.0, out_max: 1.0, curve: -4.0)
+  t_change = t_max - t_min
+  out_change = out_max - out_min
+  case curve
+  when -0.125..0.125 # use linear mapping if curve is close to zero
+    ComposableSoundGenerator.define do |t|
+      t = t.clamp(t_min, t_max)
+      out_min + out_change * (t - t_min) / t_change
+    end
+  else
+    grow = Math.exp(curve)
+    a = out_change / (1 - grow)
+    b = out_min + a
+    ComposableSoundGenerator.define do |t|
+      t = t.clamp(t_min, t_max)
+      b - (a * (grow**(t - t_min)))
+    end
   end
 end
