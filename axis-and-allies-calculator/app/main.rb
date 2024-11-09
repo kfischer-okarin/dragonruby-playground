040da3ee7816cde0bf6fa25759ed8ef66e778cee
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-11-09 19:24:28 +0900

    Add unit stats

---
+++ axis-and-allies-calculator/app/main.rb
@@ -0,0 +1,15 @@
+UNITS = {
+  infantry: { attack: 1, defense: 2, cost: 3, movement: 1 },
+  armor: { attack: 3, defense: 2, cost: 5, movement: 2 },
+  fighter: { attack: 3, defense: 4, cost: 12, movement: 4 },
+  bomber: { attack: 4, defense: 1, cost: 15, movement: 6 },
+  antiaircraft: { attack: 0, defense: 1, cost: 5, movement: 1 },
+  battleship: { attack: 4, defense: 4, cost: 24, movement: 2 },
+  aircraft_carrier: { attack: 1, defense: 3, cost: 18, movement: 2 },
+  transport: { attack: 0, defense: 1, cost: 8, movement: 2 },
+  submarine: { attack: 2, defense: 2, cost: 8, movement: 2 }
+}
+
+
+def tick(args)
+end
