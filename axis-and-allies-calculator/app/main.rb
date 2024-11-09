2d0ff9564bb49dcae73e97f4fdf85faa4240307b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2024-11-09 21:02:01 +0900

    Add attacker/defender add/remove

--- axis-and-allies-calculator/app/main.rb
+++ axis-and-allies-calculator/app/main.rb
@@ -12,4 +12,85 @@ UNITS = {
 
 
 def tick(args)
+  args.state.attackers ||= {}
+  args.state.defenders ||= {}
+  args.state.buttons ||= build_buttons(args)
+
+  handle_input(args)
+
+  render_units(args)
+  render_buttons(args)
+end
+
+ATTACKER_X = 280
+DEFENDER_X = 430
+
+def handle_input(args)
+  mouse = args.inputs.mouse
+  if mouse.click
+    clicked_button = args.state.buttons.find { |button| mouse.inside_rect?(button) }
+    clicked_button[:on_click].call if clicked_button
+  end
+end
+
+def build_buttons(args)
+  attackers = args.state.attackers
+  defenders = args.state.defenders
+
+  UNITS.keys.each_with_index.flat_map { |unit, index|
+    y = 625 - index * 40
+    [
+      {
+        x: ATTACKER_X - 50, y: y, w: 30, h: 30, type: :remove,
+        on_click: -> { attackers[unit] = [attackers[unit] - 1, 0].max }
+      },
+      {
+        x: ATTACKER_X + 20, y: y, w: 30, h: 30, type: :add,
+        on_click: -> { attackers[unit] = (attackers[unit] || 0) + 1 }
+      },
+      {
+        x: DEFENDER_X - 50, y: y, w: 30, h: 30, type: :remove,
+        on_click: -> { defenders[unit] = [defenders[unit] - 1, 0].max }
+      },
+      {
+        x: DEFENDER_X + 20, y: y, w: 30, h: 30, type: :add,
+        on_click: -> { defenders[unit] = (defenders[unit] || 0) + 1 }
+      }
+    ]
+  }
+end
+
+def render_units(args)
+  unit_name_x = 20
+  args.outputs.labels << { x: ATTACKER_X, y: 700, text: 'Attackers', anchor_x: 0.5 }
+  args.outputs.labels << { x: DEFENDER_X, y: 700, text: 'Defenders', anchor_x: 0.5 }
+  UNITS.each.with_index do |(unit, stats), i|
+    label_y = 650 - i * 40
+    line_y = 620 - i * 40
+    args.outputs.labels << { x: unit_name_x, y: label_y, text: unit_name(unit) }
+    attacker_count = args.state.attackers[unit] || 0
+    args.outputs.labels << { x: ATTACKER_X, y: label_y, text: attacker_count, anchor_x: 0.5 }
+    defender_count = args.state.defenders[unit] || 0
+    args.outputs.labels << { x: DEFENDER_X, y: label_y, text: defender_count, anchor_x: 0.5 }
+    args.outputs.lines << { x: unit_name_x, y: line_y, x2: DEFENDER_X + 50, y2: line_y }
+  end
+end
+
+def render_buttons(args)
+  args.outputs.primitives << args.state.buttons.map { |button|
+    result = [
+      button.merge(path: :pixel, r: 0, g: 0, b: 0)
+    ]
+    case button[:type]
+    when :add
+      result << { x: button[:x] + 7, y: button[:y] + 30, text: '+', size_enum: 5, r: 255, g: 255, b: 255 }
+    when :remove
+      result << { x: button[:x] + 7, y: button[:y] + 30, text: '-', size_enum: 5, r: 255, g: 255, b: 255 }
+    end
+    result
+  }
+end
+
+def unit_name(unit_symbol)
+  unit_symbol.to_s.split('_').map(&:capitalize).join(' ')
 end
